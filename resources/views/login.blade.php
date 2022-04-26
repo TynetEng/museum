8ea3b152515b56f19ed065016022cbfae6fd3a91
TynetEng <agbesuaoluwatoyin96@yahoo.com>
2022-04-26 10:11:13 +0100

    google socialite implemented

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
-    <title>Document</title>
+    <title>LOGIN</title>
 </head>
 <body>
     <div class="container-fluid">
